b63ec4067995b1c6d047fddb994700425694a447
lmandres <lmandres@yahoo.com>
2024-10-16 21:59:53 -0400

    Initial commit with working version of gsl multifit nlinear.

--- build.rs
+++ build.rs
@@ -5,6 +5,10 @@ fn main() {
         .flag("-lgslsyslib")
         .compile("gslmfnlin");
     println!("cargo::rustc-link-search=./target/debug");
+    println!("cargo::rustc-link-search=./gslsyslib/target/debug");
     println!("cargo::rustc-link-lib=gslmfnlin");
+    println!("cargo::rustc-link-lib=gsl");
+    println!("cargo::rustc-link-lib=gslcblas");
+    println!("cargo::rustc-link-lib=gslsyslib");
     println!("cargo::rerun-if-changed=src/multifit_nlinear.c");
 }
